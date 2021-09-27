ec529b3cf4508d2243aedd6978ee5a45cc386920
Christian Roessner <c@roessner.co>
2021-09-27 10:18:27 +0200

    [Project] Add all command line tests

--- config_test.go
+++ config_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-ldap/ldap/v3"
 	"os"
 	"testing"
 )
@@ -26,6 +27,36 @@ func TestConfigVerboseDebug(t *testing.T) {
 	cfg.Init([]string{"app", "server", "--verbose", "--verbose"})
 	if cfg.Verbose != logLevelDebug {
 		t.Errorf("Expected --verbose --verbose, got value=%v", cfg.Verbose)
+	}
+}
+
+func TestConfigEnvVerboseNone(t *testing.T) {
+	//goland:noinspection GoUnhandledErrorResult
+	os.Setenv("VERBOSE", "none")
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server"})
+	if cfg.Verbose != logLevelNone {
+		t.Errorf("Expected VERBOSE=%d, got value=%v", logLevelNone, cfg.Verbose)
+	}
+}
+
+func TestConfigEnvVerboseInfo(t *testing.T) {
+	//goland:noinspection GoUnhandledErrorResult
+	os.Setenv("VERBOSE", "info")
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server"})
+	if cfg.Verbose != logLevelInfo {
+		t.Errorf("Expected VERBOSE=%d, got value=%v", logLevelInfo, cfg.Verbose)
+	}
+}
+
+func TestConfigEnvVerboseDebug(t *testing.T) {
+	//goland:noinspection GoUnhandledErrorResult
+	os.Setenv("VERBOSE", "debug")
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server"})
+	if cfg.Verbose != logLevelDebug {
+		t.Errorf("Expected VERBOSE=%d, got value=%v", logLevelDebug, cfg.Verbose)
 	}
 }
 
@@ -199,7 +230,7 @@ func TestConfigHttpUseBasicAuth(t *testing.T) {
 
 func TestConfigHttpBasicAuthUsername(t *testing.T) {
 	cfg := new(CmdLineConfig)
-	cfg.Init([]string{"app", "server", "--http-basic-auth-username", "username"})
+	cfg.Init([]string{"app", "server", "--http-use-basic-auth", "--http-basic-auth-username", "username"})
 	if cfg.HttpApp.Auth.Username != "username" {
 		t.Errorf("Expected --http-basic-auth-username=username, got value=%v", cfg.HttpApp.Auth.Username)
 	}
@@ -207,7 +238,7 @@ func TestConfigHttpBasicAuthUsername(t *testing.T) {
 
 func TestConfigHttpBasicAuthPassword(t *testing.T) {
 	cfg := new(CmdLineConfig)
-	cfg.Init([]string{"app", "server", "--http-basic-auth-password", "password"})
+	cfg.Init([]string{"app", "server", "--http-use-basic-auth", "--http-basic-auth-password", "password"})
 	if cfg.HttpApp.Auth.Password != "password" {
 		t.Errorf("Expected --http-basic-auth-password=password, got value=%v", cfg.HttpApp.Auth.Password)
 	}
@@ -223,7 +254,7 @@ func TestConfigHttpUseSSL(t *testing.T) {
 
 func TestConfigHttpTLSCert(t *testing.T) {
 	cfg := new(CmdLineConfig)
-	cfg.Init([]string{"app", "server", "--http-tls-cert", "/tmp"})
+	cfg.Init([]string{"app", "server", "--http-use-ssl", "--http-tls-cert", "/tmp"})
 	if cfg.HttpApp.X509.Cert != "/tmp" {
 		t.Errorf("Expected --http-tls-cert=/tmp, got value=%v", cfg.HttpApp.X509.Cert)
 	}
@@ -231,38 +262,248 @@ func TestConfigHttpTLSCert(t *testing.T) {
 
 func TestConfigHttpTLSKey(t *testing.T) {
 	cfg := new(CmdLineConfig)
-	cfg.Init([]string{"app", "server", "--http-tls-key", "/tmp"})
+	cfg.Init([]string{"app", "server", "--http-use-ssl", "--http-tls-key", "/tmp"})
 	if cfg.HttpApp.X509.Key != "/tmp" {
 		t.Errorf("Expected --http-tls-key=/tmp, got value=%v", cfg.HttpApp.X509.Key)
 	}
 }
 
-func TestConfigEnvVerboseNone(t *testing.T) {
-	//goland:noinspection GoUnhandledErrorResult
-	os.Setenv("VERBOSE", "none")
+func TestConfigUseLDAP(t *testing.T) {
 	cfg := new(CmdLineConfig)
-	cfg.Init([]string{"app", "server"})
-	if cfg.Verbose != logLevelNone {
-		t.Errorf("Expected VERBOSE=%d, got value=%v", logLevelNone, cfg.Verbose)
+	cfg.Init([]string{"app", "server", "--use-ldap"})
+	if cfg.UseLDAP != true {
+		t.Errorf("Expected --use-ldap, got value=%v", cfg.UseLDAP)
 	}
 }
 
-func TestConfigEnvVerboseInfo(t *testing.T) {
-	//goland:noinspection GoUnhandledErrorResult
-	os.Setenv("VERBOSE", "info")
+func TestConfigLDAPServerUris(t *testing.T) {
 	cfg := new(CmdLineConfig)
-	cfg.Init([]string{"app", "server"})
-	if cfg.Verbose != logLevelInfo {
-		t.Errorf("Expected VERBOSE=%d, got value=%v", logLevelInfo, cfg.Verbose)
+	u1 := "ldap://localhost:389/"
+	u2 := "ldap://example.com:389/"
+	f1 := false
+	f2 := false
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-server-uri", u1, "--ldap-server-uri", u2})
+	for _, v := range cfg.LDAP.ServerURIs {
+		if v == u1 {
+			f1 = true
+			break
+		}
+	}
+	for _, v := range cfg.LDAP.ServerURIs {
+		if v == u2 {
+			f2 = true
+			break
+		}
+	}
+	if f1 != true && f2 != true {
+		t.Errorf("Expected --ldap-server-uri=%s --ldap-server-uri=%s, got value=%v", u1, u2, cfg.LDAP.ServerURIs)
 	}
 }
 
-func TestConfigEnvVerboseDebug(t *testing.T) {
-	//goland:noinspection GoUnhandledErrorResult
-	os.Setenv("VERBOSE", "debug")
+func TestConfigLDAPBaseDN(t *testing.T) {
 	cfg := new(CmdLineConfig)
-	cfg.Init([]string{"app", "server"})
-	if cfg.Verbose != logLevelDebug {
-		t.Errorf("Expected VERBOSE=%d, got value=%v", logLevelDebug, cfg.Verbose)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-basedn", "o=org"})
+	if cfg.LDAP.BaseDN != "o=org" {
+		t.Errorf("Expected --ldap-basedn=o=org, got value=%v", cfg.LDAP.BaseDN)
+	}
+}
+
+func TestConfigLDAPBindDN(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-binddn", "cn=admin,o=org"})
+	if cfg.LDAP.BindDN != "cn=admin,o=org" {
+		t.Errorf("Expected --ldap-binddn=cn=admin,o=org, got value=%v", cfg.LDAP.BindDN)
+	}
+}
+
+func TestConfigLDAPBindPW(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-bindpw", "password"})
+	if cfg.LDAP.BindPW != "password" {
+		t.Errorf("Expected --ldap-bindpw=password, got value=%v", cfg.LDAP.BindPW)
+	}
+}
+
+func TestConfigLDAPFilter(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-filter", "(objectClass=*)"})
+	if cfg.LDAP.Filter != "(objectClass=*)" {
+		t.Errorf("Expected --ldap-filter=(objectClass=*), got value=%v", cfg.LDAP.Filter)
+	}
+}
+
+func TestConfigLDAPResultAttribute(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-result-attribute", "mail"})
+	if cfg.LDAP.ResultAttr[0] != "mail" {
+		t.Errorf("Expected --ldap-result-attribute=mail, got value=%v", cfg.LDAP.ResultAttr)
+	}
+}
+
+func TestConfigLDAPStartTLS(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-starttls"})
+	if cfg.LDAP.StartTLS != true {
+		t.Errorf("Expected --ldap-starttls, got value=%v", cfg.LDAP.StartTLS)
+	}
+}
+
+func TestConfigLDAPTLSSkipVerify(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-skip-tls-verify"})
+	if cfg.LDAP.TLSSkipVerify != true {
+		t.Errorf("Expected --ldap-tls-skip-verify, got value=%v", cfg.LDAP.TLSSkipVerify)
+	}
+}
+
+func TestConfigLDAPTLSClientCert(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-tls-client-cert", "/tmp"})
+	if cfg.LDAP.TLSClientCert != "/tmp" {
+		t.Errorf("Expected --ldap-tls-client-cert=/tmp, got value=%v", cfg.LDAP.TLSClientCert)
+	}
+}
+
+func TestConfigLDAPTLSClientKey(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-tls-client-key", "/tmp"})
+	if cfg.LDAP.TLSClientKey != "/tmp" {
+		t.Errorf("Expected --ldap-tls-client-key=/tmp, got value=%v", cfg.LDAP.TLSClientKey)
+	}
+}
+
+func TestConfigLDAPSASLExternal(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-sasl-external"})
+	if cfg.LDAP.SASLExternal != true {
+		t.Errorf("Expected --ldap-sasl-external, got value=%v", cfg.LDAP.SASLExternal)
+	}
+}
+
+func TestConfigLDAPScopeBase(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-scope", BASE})
+	if cfg.LDAP.Scope != ldap.ScopeBaseObject {
+		t.Errorf("Expected --ldap-scope=base (%d), got value=%v", ldap.ScopeBaseObject, cfg.LDAP.Scope)
+	}
+}
+
+func TestConfigLDAPScopeOne(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-scope", ONE})
+	if cfg.LDAP.Scope != ldap.ScopeSingleLevel {
+		t.Errorf("Expected --ldap-scope=one (%d), got value=%v", ldap.ScopeSingleLevel, cfg.LDAP.Scope)
+	}
+}
+
+func TestConfigLDAPScopeSub(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--use-ldap", "--ldap-scope", SUB})
+	if cfg.LDAP.Scope != ldap.ScopeWholeSubtree {
+		t.Errorf("Expected --ldap-scope=sub (%d), got value=%v", ldap.ScopeWholeSubtree, cfg.LDAP.Scope)
+	}
+}
+
+func TestConfigRunActions(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--run-actions"})
+	if cfg.RunActions != true {
+		t.Errorf("Expected --run-actions, got value=%v", cfg.RunActions)
+	}
+}
+
+func TestConfigRunActionOperator(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--run-actions", "--run-action-operator"})
+	if cfg.RunActionOperator != true {
+		t.Errorf("Expected --run-action-operator, got value=%v", cfg.RunActionOperator)
+	}
+}
+
+func TestConfigOperatorTo(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--run-actions", "--run-action-operator", "--operator-to", "test"})
+	if cfg.EmailOperatorTo != "test" {
+		t.Errorf("Expected --operator-to=test, got value=%v", cfg.EmailOperatorTo)
+	}
+}
+
+func TestConfigOperatorFrom(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--run-actions", "--run-action-operator", "--operator-from", "test"})
+	if cfg.EmailOperatorFrom != "test" {
+		t.Errorf("Expected --operator-from=test, got value=%v", cfg.EmailOperatorFrom)
+	}
+}
+
+func TestConfigOperatorSubject(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--run-actions", "--run-action-operator", "--operator-subject", "test"})
+	if cfg.EmailOperatorSubject != "test" {
+		t.Errorf("Expected --operator-subject=test, got value=%v", cfg.EmailOperatorSubject)
+	}
+}
+
+func TestConfigOperatorMessageCT(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--run-actions", "--run-action-operator", "--operator-message-ct", "text/html"})
+	if cfg.EmailOperatorMessageCT != "text/html" {
+		t.Errorf("Expected --operator-message-ct=text/html, got value=%v", cfg.EmailOperatorMessageCT)
+	}
+}
+
+func TestConfigOperatorMessagePath(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--run-actions", "--run-action-operator", "--operator-message-path", "/tmp"})
+	if cfg.EmailOperatorMessagePath != "/tmp" {
+		t.Errorf("Expected --operator-message-path=/tmp, got value=%v", cfg.EmailOperatorMessagePath)
+	}
+}
+
+func TestConfigMailServer(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--mail-server", "mail.google.com"})
+	if cfg.MailServer != "mail.google.com" {
+		t.Errorf("Expected --mail-server=mail.google.com, got value=%v", cfg.MailServer)
+	}
+}
+
+func TestConfigMailHelo(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--mail-helo", "localhost.localdomain"})
+	if cfg.MailHelo != "localhost.localdomain" {
+		t.Errorf("Expected --mail-helo=localhost.localdomain, got value=%v", cfg.MailHelo)
+	}
+}
+
+func TestConfigMailPort(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--mail-port", "465"})
+	if cfg.MailPort != 465 {
+		t.Errorf("Expected --mail-port=465, got value=%v", cfg.MailPort)
+	}
+}
+
+func TestConfigMailUsername(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--mail-username", "username"})
+	if cfg.MailUsername != "username" {
+		t.Errorf("Expected --mail-username=username, got value=%v", cfg.MailUsername)
+	}
+}
+
+func TestConfigMailPassword(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--mail-password", "password"})
+	if cfg.MailPassword != "password" {
+		t.Errorf("Expected --mail-password=password, got value=%v", cfg.MailPassword)
+	}
+}
+
+func TestConfigMailSSL(t *testing.T) {
+	cfg := new(CmdLineConfig)
+	cfg.Init([]string{"app", "server", "--mail-ssl"})
+	if cfg.MailSSL != true {
+		t.Errorf("Expected --mail-ssl, got value=%v", cfg.MailSSL)
 	}
 }
